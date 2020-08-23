45f0aff213ae9e7baed73e2da79d99424f2b4aa4
guesszjy <1010183820@qq.com>
2020-08-24 01:05:59 +0800

    支持math动态库的生成和使用

--- common.mk
+++ common.mk
@@ -13,31 +13,38 @@ LIB_OBJ_DIR = $(BUILD_ROOT)/app/lib_obj
 $(shell mkdir -p $(LIB_OBJ_DIR))
 
 LIB_DIR = $(BUILD_ROOT)/lib
-
 OBJ_DIR = $(LINK_OBJ_DIR)
 ifneq ("$(LIB)","")
 OBJ_DIR = $(LIB_OBJ_DIR)
+endif
+ifneq ("$(DLL)","")
+OBJ_DIR = $(LIB_OBJ_DIR)
+PIC = -fPIC
 endif
 
 OBJS := $(addprefix $(OBJ_DIR)/,$(OBJS))
 DEPS := $(addprefix $(DEP_DIR)/,$(DEPS))
 LIB :=  $(addprefix $(LIB_DIR)/,$(LIB))
-LINK_OBJ = $(wildcard $(LINK_OBJ_DIR)/*.o)
-LINK_OBJ +=$(OBJS)
+DLL := $(addprefix $(LIB_DIR)/,$(DLL))
 
-LIB_DEP = $(wildcard $(LIB_DIR)/*.a)
+LINK_OBJ = $(wildcard $(LINK_OBJ_DIR)/*.o)
+LINK_OBJ += $(OBJS)
+
+LIB_DEP = $(wildcard $(LIB_DIR)/*.a) $(wildcard $(LIB_DIR)/*.so)
 LINK_LIB_NAME = $(patsubst lib%,-l%,$(basename $(notdir $(LIB_DEP))))#模式替换
 
-all: $(DEPS) $(OBJS) $(LIB) $(BIN)
+all: $(DEPS) $(OBJS) $(LIB) $(DLL) $(BIN)
 ifneq ("$(wildcard $(DEPS))","")	
 include $(DEPS)
 endif
-$(BIN):$(LINK_OBJ) $(LIB_DEP)
+$(BIN):$(LINK_OBJ)
 	gcc -o $@ $^ -L$(LIB_DIR) $(LINK_LIB_NAME)
 $(LIB): $(OBJS)
 	ar rcs $@ $^	
+$(DLL): $(OBJS)
+	gcc -shared -o $@ $^
 $(OBJ_DIR)/%.o:%.c
-	gcc -I $(HEAD_PATH) -o $@ -c $(filter %.c,$^)
+	gcc -I $(HEAD_PATH) -o $@ -c $(PIC) $(filter %.c,$^)
 $(DEP_DIR)/%.d:%.c
 	gcc -I $(HEAD_PATH) -MM $(filter %.c,$^) | sed 's,\(.*\)\.o[ :]*,$(OBJ_DIR)/\1.o $@:,g' > $@
 clean:
---
+++ math/math.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "math.h"
+
+void math_dll_init()
+{
+	printf("math dll init...\n");
+}
